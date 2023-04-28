59408363147198d86e466d7f7fd28ad2e10ed707
neyserht <neyser.nht@gmail.com>
2023-04-28 14:10:27 +0200

    comenzando

---
+++ 28.04.2023/js/formulario.js
@@ -0,0 +1,13 @@
+const b1 = this.document.getElementById("btn1");
+
+//b1.addEventListener('click', enviar);
+
+console.log(b1);
+
+function enviar(){
+     console.log("Dentro");
+}
+
+//b1.addEventListener("click", enviar);
+
+//b1.addEventListener("click", console.log("hola"));
